2f152e0130f4a98ad10f1401b068836f2afad69e
kmurata798 <kmurata798@gmail.com>
2020-03-02 13:55:26 -0800

    [Add] added last tests

--- Code/set_test.py
+++ Code/set_test.py
@@ -56,8 +56,40 @@ class SetTest(unittest.TestCase):
         set1 = HashSet(elements)
         set2 = HashSet(elements2)
         set3 = HashSet(elements3)
-        self.assertCountEqual(set1.union(set2).hash.values(), ['A', 'B', 'D', 'E', 'G', 'I', 'W', 'X', 'Y', 'Z'])  # item order does not matter
+        self.assertCountEqual(set1.union(set2).hash.values(), ['A', 'B', 'D', 'E', 'G', 'I', 'W', 'X', 'Y', 'Z'])  # Item order does not matter
         self.assertCountEqual(set1.union(set3).hash.values(), ['C', 'M', 'N', 'V', 'W', 'X', 'Y', 'Z'])  # Item order does not matter
+
+    def test_intersection(self):
+        elements = ['2', 'A', 'B', 'C']
+        elements2 = ['0', 'A', 'C', 'E', 'X', '2']
+        elements3 = ['B', 'J', 'L', 'K', 'C', '2']
+        set1 = HashSet(elements)
+        set2 = HashSet(elements2)
+        set3 = HashSet(elements3)
+        self.assertCountEqual(set1.intersection(set2).hash.values(), ['A', 'C', '2'])  # Item order does not matter
+        self.assertCountEqual(set1.intersection(set3).hash.values(), ['B', 'C', '2']) # Item order does not matter
+
+    def test_difference(self):
+        elements = ['5', '6', '8', '10', '1']
+        elements2 = ['1', '5', '6', '7', '2', '9']
+        elements3 = ['2', '4', '6', '9', '10']
+        set1 = HashSet(elements)
+        set2 = HashSet(elements2)
+        set3 = HashSet(elements3)
+        self.assertCountEqual(set1.difference(set2).hash.values(), ['8', '10'])  # Item order does not matter
+        self.assertCountEqual(set1.difference(set3).hash.values(), ['5', '8', '1'])  # Item order does not matter
+
+    def test_is_subset(self):
+        elements = ['A', 'G', 'R']
+        elements2 = ['G', 'D', 'R', 'P', 'A', 'W']
+        elements3 = ['I', 'O', 'S', 'K', 'M', 'Z']
+        set1 = HashSet(elements)
+        set2 = HashSet(elements2)
+        set3 = HashSet(elements3)
+        assert set1.is_subset(set2) is True
+        assert set1.is_subset(set3) is False
+        assert set2.is_subset(set3) is False
+
 
 if __name__ == '__main__':
     unittest.main()
