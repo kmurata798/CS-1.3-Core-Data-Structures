11ab28e9837dab9d48164728681e1c50514aac2b
kmurata798 <kmurata798@gmail.com>
2020-03-01 16:08:14 -0800

    [Add] docstrings

--- Code/set.py
+++ Code/set.py
@@ -3,29 +3,44 @@ from hashtable import HashTable
 
 class HashSet:
     def __init__(self, elements=None):
+        """initialize a new empty set structure, and 
+        add each element if a sequence is given"""
         self.hash = HashTable()
         self.size = 0
 
     def contains(self, element):
+        """return a boolean indicating whether element is 
+        in this set"""
         return self.hash.contains(element)
     
     def add(self, element):
+        """add element to this set, if not present already"""
         if self.contains(element):
             return self.hash.set(element, element)
             self.size += 1
     
     def remove(self, element):
+        """remove element from this set, if present, 
+        or else raise KeyError"""
         if self.contains(element):
             return self.hash.delete(element)
             self.size -= 1
 
     def union(self, other_set):
+        """return a new set that is the union of this set 
+        and other_set"""
         
     def intersection(self, other_set):
+        """return a new set that is the intersection of 
+        this set and other_set"""
         
     def difference(self, other_set):
+        """return a new set that is the difference of 
+        this set and other_set"""
         
     def is_subset(self, other_set):
+        """return a boolean indicating whether other_set 
+        is a subset of this set"""
         
 
 
