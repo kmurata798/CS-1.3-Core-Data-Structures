f91bbd972f03b13d2aa34d90087427ca456e08a9
kmurata798 <kmurata798@gmail.com>
2020-03-02 13:33:17 -0800

    [Add] added contains test

--- Code/set_test.py
+++ Code/set_test.py
@@ -13,6 +13,15 @@ class SetTest(unittest.TestCase):
         set = HashSet(elements)
         assert set.size is 5
     
+    def test_contains(self):
+        elements = ['K', 'Q', 'J', 'O']
+        set = HashSet(elements)
+        assert set.contains('K') is True
+        assert set.contains('J') is True
+        assert set.contains('Q') is True
+        assert set.contains('C') is False
+        assert set.contains('W') is False
+
     
 
 if __name__ == '__main__':
